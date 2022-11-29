6535efbd3ba2af660607f7d83e23fec48188b744
YUNSO99 <l01090259914@gmail.com>
2022-11-29 19:18:07 +0900

    main.c input.c 수정

---
+++ input.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+void Input(){
+    printf("짜잔~~ 당신만의 시크릿산타! 마니또를 시작하려면 입력을 해주셔야겠죠?\n
+            입력은 {입력: 홍길동} <- 와 같이 성함을 적어주시는 거랍니다??\n");
+    for (int i=0;)
+    scanf("%d")
+
+
+
+    int num = 0;
+    Student *man = 0;
+    int s = 0;
+
+    printf("추가할 학생 번호(1~%d): ", people_num);
+    scanf_s("%d", &num);
+
+    if (IsAvailNum(num) == 0)//유효한 번호가 아닐 때
+    {
+        printf("범위를 벗어난 학생 번호입니다.\n");
+        return;
+    }
+
+    if (mani[num - 1].num)//이미 번호를 설정한 상태일 때
+    {
+        printf("이미 추가하였습니다\n");
+        return;
+    }
+
+    //stues에는 1번 학생 데이터를 관리하는 메모리 주소
+    //따라서 mani+(num-1)은 num번 학생 데이터를 관리하는 메모리 주소
+    man = mani + (num - 1);
+    man->num = num;
+    printf("이름: ");
+    scanf_s("%s", man->name, sizeof(man->name));
+    return 0;
+}
--- main.c
+++ main.c
@@ -1,6 +1,69 @@
 #include <stdio.h>
+#include <memory.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(){
-    
+#define MAX_NLEN    20 //최대 이름 길이
+#define MAX_SUBJECT 3 //과목 수
+
+struct Manito{//구조체 정의
+    char name[MAX_NLEN + 1];//이름
+};
+
+Manito *mani;
+int people_num;
+
+
+void Initialize(); //초기화
+void Run();
+int main(void){
+
+    Initialize();
+    Run();
     return 0;
 }
+
+void Initialize()
+{
+    int i = 0;
+    int s = 0;
+
+    printf("두구두구 마니또를 시작할 건데요!! 잠깐!!!!! 몇 명이서 진행하시죠?!\n");
+    scanf_s("%d", &people_num);
+    mani = (Manito *)malloc(sizeof(Manito)*people_num);//동적 배열 메모리 할당
+    memset(mani, 0, sizeof(Manito)*people_num);//메모리 초기화
+}
+
+int SelectMenu();//메뉴 출력 및 선택
+void Input();
+void Search();
+void Match();
+void Output();
+
+void Run()
+{
+    int key = 0;
+    while ((key = SelectMenu()) != 0)//선택한 메뉴가 0이 아니면 반복
+    {
+        switch (key)//선택한 키에 따라 기능 수행
+        {
+        case 1: Input(); break;
+        case 2: Search(); break;
+        case 3: Match(); break;
+        case 4: Output(); break;
+        default: printf("삐빅-- 틀렸습니다. 다시 하셔야겠는디요?\n"); break;
+        }
+    }
+    printf("그럼 종료할게요~ 다음에 만나요~~~~~\n");
+}
+
+int SelectMenu(){
+    int key = 0;
+    printf("마니또 프로그램인뎁쇼?\n");
+    printf("종료: 0\n 입력: 1\n 검색:2\n 맞추기:3\n 출력:4\n");
+    scanf("%d", &key);
+    return key;
+}
+
+
+
